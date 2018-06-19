4b06b7f5ba84614c8e7f243d60e4b85d0c4b7590
Chris May <chris.may@blackpepper.co.uk>
2018-06-19 11:15:19 +0100

    Add a FAB

--- src/pages/EventListPage.tsx
+++ src/pages/EventListPage.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { List } from "@material-ui/core";
+import { List, Button } from "@material-ui/core";
 import Event from "../components/Event";
 import AppMenuBar from "../components/AppMenuBar";
-
+import AddIcon from "@material-ui/icons/Add";
 const EventListPage = () => {
   return (
     <div>
-      <AppMenuBar/>          
+      <AppMenuBar />
       <List>
         <Event id="1" label="01/06/2018" />
         <Event id="2" label="02/06/2018" />
@@ -14,6 +14,14 @@ const EventListPage = () => {
         <Event id="4" label="04/06/2018" />
         <Event id="5" label="05/06/2018" />
       </List>
+      <Button
+          variant="fab"
+          color="primary"
+          aria-label="add"
+          style={{ position: "absolute", bottom: 80, right: 20 }}
+        >
+          <AddIcon />
+        </Button>
     </div>
   );
 };
